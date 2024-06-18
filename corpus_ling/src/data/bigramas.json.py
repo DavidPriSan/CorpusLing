a66d2645e10ab8b13282d7a51b366df071ceb451
DavidPriSa <david.prisan18@gmail.com>
2024-06-18 13:35:05 +0200

    Año y país
    Añadidos datos de año y país al azar para cada palabra

--- corpus_ling/src/data/bigramas.json.py
+++ corpus_ling/src/data/bigramas.json.py
@@ -1,4 +1,4 @@
-import nltk, re, string, collections, json, kaggle, sys
+import nltk, re, string, collections, json, kaggle, sys, random
 from nltk.util import ngrams
 
 #Autenticacion Kaggle API
@@ -8,7 +8,7 @@ from nltk.util import ngrams
 #kaggle.api.dataset_download_files('rtatman/120-million-word-spanish-corpus', path='./src/data/', unzip=True)
 
 #Carga y formateo de datos
-with open ("./src/data/spanishText_110000_115000", "r", encoding="latin-1") as file:
+with open ("./src/data/spanishText_205000_210000", "r", encoding="latin-1") as file:
     text = file.read()
     
 text = text.lower()
@@ -22,6 +22,8 @@ text = re.sub(punctuationNoPeriod, " ", text)
 
 text = re.sub(r"\d+", "", text)
 
+paises = ['España', 'México', 'Colombia', 'Argentina', 'Perú', 'Venezuela', 'Chile', 'Guatemala', 'Ecuador', 'Bolivia', 'Cuba']
+
 #Palabras individuales
 tokenized = text.split()
 
@@ -30,10 +32,10 @@ bigrams = [{} for i in range(len(tokenized))]
 i = 0
 for j in tokenized:
 	if i < len(tokenized)-1:
-		bigrams[i] = {"word": j, "bigram": tokenized[i + 1]}
+		bigrams[i] = {"word": j, "bigram": tokenized[i + 1], "year": random.randint(1990,2025), "country": random.choice(paises)}
 		i += 1
 	else:
-		bigrams[i] = {"word": j, "bigram": ""}
+		bigrams[i] = {"word": j, "bigram": "", "year": random.randint(1990,2025), "country": random.choice(paises)}
 		i += 1
 
 #Devolver resultado en json
